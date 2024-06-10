bd34a8ca73901514dc01a9f017e8cf4a0719e855
KramYengir <kramyengir@gmail.com>
2024-06-10 13:30:46 +0100

    basic navbar style and dropdown complete

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,6 +2,15 @@ import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 import Logo from "../img/logo.svg";
+import { Avatar, AvatarImage, AvatarFallback } from "./ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
 
 const Navbar = () => {
   return (
@@ -9,6 +18,31 @@ const Navbar = () => {
       <Link href={"/"}>
         <Image src={Logo} alt="RigneyMade Logo" width={40}></Image>
       </Link>
+
+      <DropdownMenu>
+        <DropdownMenuTrigger>
+          <Avatar>
+            <AvatarImage
+              src="https://github.com/shadcn.png"
+              alt="shadcn avatar"
+            />
+            <AvatarFallback className="text-black">RM</AvatarFallback>
+          </Avatar>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuLabel>My Account</DropdownMenuLabel>
+          <DropdownMenuItem>
+            <Link href="/profile">Profile</Link>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            <Link href="/settings">Settings</Link>
+          </DropdownMenuItem>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>
+            <Link href="/auth">Logout</Link>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
     </div>
   );
 };
