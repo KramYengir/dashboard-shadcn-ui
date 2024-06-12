5d148cec2dbda9e5c927d897f81e8a603841752f
KramYengir <kramyengir@gmail.com>
2024-06-12 16:36:56 +0100

    add form for editing (display only)

---
+++ app/(main)/posts/edit/[id]/page.tsx
@@ -0,0 +1,120 @@
+"use client";
+import BackButton from "@/components/BackButton";
+import posts from "@/data/posts";
+import { Post } from "@/types/posts";
+import React from "react";
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Textarea } from "@/components/ui/textarea";
+import data from "@/data/analytics";
+
+interface EditPostPageProps {
+  params: {
+    id: number;
+  };
+}
+
+const schema = z.object({
+  title: z.string().min(1, "Title is required"),
+  body: z.string().min(1, "Body is required"),
+  author: z.string().min(1, "Author is required"),
+  date: z.string().min(1, "Date is required"),
+});
+
+const EditPostPage = ({ params }: EditPostPageProps) => {
+  const { id } = params;
+  const post = posts.find((post) => post.id == id);
+
+  const form = useForm<z.infer<typeof schema>>({
+    resolver: zodResolver(schema),
+    defaultValues: post,
+  });
+
+  const handleSubmit = (data: z.infer<typeof schema>) => {
+    console.log(data);
+  };
+
+  return (
+    <>
+      <BackButton text="Back to Posts" href="/posts" />
+      <h3 className=" text-2xl mb-4">Edit Post</h3>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(handleSubmit)} className="space-y-4">
+          <FormField
+            control={form.control}
+            name="title"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="uppercase text-xs font-bold text-zinc-500 dark:text-white">
+                  Title
+                </FormLabel>
+                <FormControl>
+                  <Input
+                    className=" bg-slate-100 dark:bg-slate-500 border-0 focus-visible:ring-0 text-black dark:text-white focus-visible:ring-offset-0"
+                    placeholder="Enter Title..."
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="body"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="uppercase text-xs font-bold text-zinc-500 dark:text-white">
+                  Body
+                </FormLabel>
+                <FormControl>
+                  <Textarea
+                    className=" bg-slate-100 dark:bg-slate-500 border-0 focus-visible:ring-0 text-black dark:text-white focus-visible:ring-offset-0"
+                    placeholder="Enter Body..."
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="author"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="uppercase text-xs font-bold text-zinc-500 dark:text-white">
+                  Author
+                </FormLabel>
+                <FormControl>
+                  <Input
+                    className=" bg-slate-100 dark:bg-slate-500 border-0 focus-visible:ring-0 text-black dark:text-white focus-visible:ring-offset-0"
+                    placeholder="Enter Author's Name..."
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button className=" w-full dark:bg-slate-800 dark:text-white">
+            Save Changes
+          </Button>
+        </form>
+      </Form>
+    </>
+  );
+};
+
+export default EditPostPage;
