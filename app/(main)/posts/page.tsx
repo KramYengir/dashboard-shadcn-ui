893cbec701f55c3ce09b7c8f021bf11a476c29c5
KramYengir <kramyengir@gmail.com>
2024-06-12 15:48:11 +0100

    add pagination (display only)

--- app/(main)/posts/page.tsx
+++ app/(main)/posts/page.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import PostTable from "@/components/posts/PostTable";
 import BackButton from "@/components/BackButton";
+import PostPagination from "@/components/posts/PostPagination";
 
 const PostsPage = () => {
   return (
     <>
       <BackButton text="Back to Dashboard" href="/" />
-      <PostTable />;
+      <PostTable />
+      <PostPagination />
     </>
   );
 };
---
+++ components/posts/PostPagination.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "@/components/ui/pagination";
+
+const PostPagination = () => {
+  return (
+    <Pagination>
+      <PaginationContent>
+        <PaginationItem>
+          <PaginationPrevious href="#" />
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationLink href="#">1</PaginationLink>
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationLink href="#">2</PaginationLink>
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationLink href="#">3</PaginationLink>
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationEllipsis />
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationNext href="#" />
+        </PaginationItem>
+      </PaginationContent>
+    </Pagination>
+  );
+};
+
+export default PostPagination;
