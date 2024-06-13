fa4d4d8d2ffb2b4f1e5a3d447f9a1ff13117635f
KramYengir <kramyengir@gmail.com>
2024-06-13 14:08:14 +0100

    add theme toggle

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,9 +1,13 @@
 import React from "react";
+import { ThemeToggle } from "@/components/ThemeToggle";
 
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className=" h-[100vh] flex items-center justify-center relative">
       {children}
+      <div className="absolute bottom-5 right-5">
+        <ThemeToggle />
+      </div>
     </div>
   );
 };
